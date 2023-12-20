0e35bebda507a5d67be509f224d00a77aad4e711
Daniel-Pacer <daniel-pacer@Daniel-Pacers-MacBook-Pro.local>
2023-12-20 09:19:58 +0800

    fix model typo

--- backend/scrapers/models.py
+++ backend/scrapers/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class CoffeeShop(models.Model):
     shop_name = models.CharField(max_length=200)
     shop_address = models.CharField(max_length=255)
-    shop_coordinatez_latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    shop_coordinatez_longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    shop_coordinate_latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    shop_coordinate_longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 
     def __str__(self):
         return self.shop_name
